1965785be3e6f03b5e48e20e3b5490d5008d76bc
Raul Vitor de Resende <103119614+raulvitor84@users.noreply.github.com>
2024-03-16 17:20:25 -0300

    Update matematica.h

--- libmatematica/include/matematica.h
+++ libmatematica/include/matematica.h
@@ -1,7 +1,6 @@
 #ifndef MATEMATICA_H
 #define MATEMATICA_H
 
-
 float calcular_seno(float angulo_radianos);
 float calcular_cosseno(float angulo_radianos);
 float calcular_tangente(float angulo_radianos);
